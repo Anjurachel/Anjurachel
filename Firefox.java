690748ffdae749d0b4d9269f00e2924a835e07d8
Anjurachel <109274204+Anjurachel@users.noreply.github.com>
2022-07-14 13:31:08 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class Firefox {
+	float version=100.2f;
+	private void addNumber() {
+		float version =100.2f;
+		String name = "anju";
+		int year = 2022;
+		System.out.println(version);
+		System.err.println(name);
+		System.out.println(year);
+	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Firefox fox=new Firefox();
+	fox.addNumber();
+	
+
+	}
+
+}
