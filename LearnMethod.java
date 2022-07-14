2b5b6302181455f01a7180c864fccc2ee8af74af
Anjurachel <109274204+Anjurachel@users.noreply.github.com>
2022-07-14 13:30:17 +0530

    Add files via upload

---
+++ LearnMethod.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class LearnMethod {
+int num=12;
+public void addNumber() {
+	int a=12;
+	int b=12;
+	int sum =a+b;
+	System.out.println(sum);
+	System.err.println(sum);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LearnMethod math = new LearnMethod();
+System.out.println(math.num);
+math.addNumber();
+	}
+
+}
